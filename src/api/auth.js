48b71c5fb5031afa4f07fd812ccf117e52bd29b7
Faleeeee <letranthuanphat111@gmail.com>
2024-09-19 23:34:03 +0700

    Folder API to Call

---
+++ src/api/auth.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+const registerUser = async (userData) => {
+    try {
+        const response = await axios.post('http://localhost:3002/api/v1/auth/register', userData);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const verify = async (email, code) => {
+    try {
+        const response = await axios.post('http://localhost:3002/api/v1/auth/verify', {
+            email: email,
+            code: code
+        });
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const loginUser = async (email, password) => {
+    try {
+        const response = await axios.post('http://localhost:3002/api/v1/auth/login', {
+            email: email,
+            password: password
+        });
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export { registerUser };
+export { verify };
+export { loginUser };
